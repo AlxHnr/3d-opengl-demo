01ccd82fd0bcad336336dfaeb786e041236ee0c3
Alexander Heinrich <alxhnr@users.noreply.github.com>
2017-05-05 14:38:22 +0200

    Added whitespaces

--- src/uniform.nim
+++ src/uniform.nim
@@ -14,17 +14,20 @@ proc getUniformLocation(program: ShaderProgram, name: string): GLint =
 proc getUniformLocationFloat*(program: ShaderProgram, name: string):
                               UniformLocationFloat =
   program.getUniformLocation(name).UniformLocationFloat
+
 proc updateWith*(location: UniformLocationFloat, value: float) =
   glUniform1f(location.GLint, value)
 
 proc getUniformLocationVec3*(program: ShaderProgram, name: string):
                              UniformLocationVec3 =
   program.getUniformLocation(name).UniformLocationVec3
+
 proc updateWith*(location: UniformLocationVec3; vector: Vector3d) =
   glUniform3f(location.GLint, vector.x, vector.y, vector.z)
 
 proc getUniformLocationMat4*(program: ShaderProgram, name: string):
                              UniformLocationMat4 =
   program.getUniformLocation(name).UniformLocationMat4
+
 proc updateWith*(location: UniformLocationMat4, matrix: var Matrix4) =
   glUniformMatrix4fv(location.GLint, 1, GL_FALSE, matrix[0].addr)
