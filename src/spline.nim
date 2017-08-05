672a2203efcc8e40c89716c58f2c3abb12117360
Alexander Heinrich <alxhnr@users.noreply.github.com>
2017-08-05 21:39:12 +0200

    Improved spline implementation

--- src/spline.nim
+++ src/spline.nim
@@ -11,8 +11,7 @@ proc newSpline*(points: openArray[Vector3d]): Spline =
   assert(points.len > 2)
 
   result.x_values = newSeq[float](points.len)
-  result.values = newSeq[SplineValues](points.len - 1)
-
+  result.values = newSeq[SplineValues](points.len)
   var helpers = newSeq[SplineHelperValues](points.len)
 
   for i, point in points:
@@ -20,10 +19,10 @@ proc newSpline*(points: openArray[Vector3d]): Spline =
     helpers[i].a = point.y
 
   # Compute clamped cubic spline.
-  for i in 0..<points.len - 2:
+  for i in 0..<points.len - 1:
     helpers[i].h = result.x_values[i + 1] - result.x_values[i]
 
-  for i in 1..<points.len - 2:
+  for i in 1..<points.len - 1:
     helpers[i].alpha =
       3.0/helpers[i].h * (helpers[i + 1].a - helpers[i].a) -
       3.0/helpers[i - 1].h * (helpers[i].a - helpers[i - 1].a)
@@ -32,7 +31,7 @@ proc newSpline*(points: openArray[Vector3d]): Spline =
   helpers[0].mu = 0
   helpers[0].z  = 0
 
-  for i in 1..<points.len - 2:
+  for i in 1..<points.len - 1:
     helpers[i].ll =
       2.0 * (result.x_values[i + 1] - result.x_values[i - 1]) -
       helpers[i - 1].h * helpers[i - 1].mu
@@ -40,25 +39,25 @@ proc newSpline*(points: openArray[Vector3d]): Spline =
     helpers[i].z =
       (helpers[i].alpha - helpers[i - 1].h * helpers[i - 1].z)/helpers[i].ll
 
-  helpers[points.len - 2].ll = 1
-  helpers[points.len - 2].c  = 0
-  helpers[points.len - 2].z  = 0
+  helpers[points.len - 1].ll = 1
+  helpers[points.len - 1].z  = 0
+  helpers[points.len - 1].c  = 0
 
-  for i in countdown(points.len - 3, 0):
+  for i in countdown(points.len - 2, 0):
     helpers[i].c = helpers[i].z - helpers[i].mu * helpers[i + 1].c
     helpers[i].b =
       (helpers[i + 1].a - helpers[i].a)/helpers[i].h -
-      helpers[i].h * (helpers[i + 1].c + 2 * helpers[i].c)/3
-    helpers[i].d = (helpers[i + 1].c - helpers[i].c)/(3 * helpers[i].h)
+      helpers[i].h * (helpers[i + 1].c + 2.0 * helpers[i].c)/3.0
+    helpers[i].d = (helpers[i + 1].c - helpers[i].c)/(3.0 * helpers[i].h)
 
-  for i in 0..<points.len - 2:
+  for i in 0..<points.len - 1:
     result.values[i].a = helpers[i].a
     result.values[i].b = helpers[i].b
     result.values[i].c = helpers[i].c
     result.values[i].d = helpers[i].d
 
 proc updateSplineLocations*(U: UniformLocations, spline: Spline) =
-  assert(spline.values.len == 3)
+  assert(spline.values.len == 4)
   assert(spline.x_values.len == 4)
 
   U.splineData.updateWith(matrix3d(
